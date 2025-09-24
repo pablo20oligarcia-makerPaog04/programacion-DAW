a26c62fba7b9ece2e2c202fb490c6b8e2f58312b
Paog04 <pablo20oligarcia@gmail.com>
2025-09-24 14:21:34 +0200

    Clase1.1

--- app.java
+++ app.java
@@ -1,16 +1,20 @@
 public class app {
-   /*  public static void hacernada() {
+    static final double PI=3.141592;//es una constante
+ /*     public static void hacernada() {
         System.out.println("estoy en hacer nada");
         int altura =2;
         int diametro=5;
         System.out.println("El area es (altura)*(diametro) ");
     }
-    int fora=0;*/
-    /*public static void main(String[] args) throws Exception {
+    int fora=0;
+    public static void main(String[] args) throws Exception {
         System.out.println("Hellow, word!");
         //hacernada();
-    }*/
+    }
+ */
+    //public boolean static logico=true;
     public static void main(String[] args) {
+        //tipo de identificador = Valor
         String cadena="me llamo Pablo";
         int edad=25;
         double salario=20.128234234;
@@ -18,6 +22,7 @@ public class app {
         int y=2;
         int z=3;*/
        /*  int x=1,y=2,z=3;
+       Defino una variable y la imprimo
 
         int variable=0;
         System.out.println(variable);
@@ -25,11 +30,34 @@ public class app {
 
         variable=33;
         System.out.println(variable);
-*/
+        
         int cuenta1= (1+2+3)/3;
         double cuenta2= (1+2+3)/3.0;
+        int a=(int)2.6;
+        
+        System.out.println(a);
+    */
 
-        System.out.println(cuenta1);
-        System.out.println(cuenta2);
+        boolean logico=true; //false
+       // System.out.println(logico);
+
+        //int logico=1;//ya existe "logico" y no se puede utilizar 
+
+        
+       // PI=0.234555; //no se puede cambiar un aconstante
+        
+
+        int b='b';//un caracter se guarda en un enter segun codigo ASCII
+        char c=98;
+        System.out.println(b+""+c);
+    }
+
+    /**Progarama para impromir texto
+     * @author Pablo Olivares García
+     * @param texto a imprimir
+     * @version 1.0
+     */
+    public static void imprimirtexto(String texto){
+        boolean logico=false;
     }
 }
