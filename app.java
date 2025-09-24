26589e7ab178633d08336f38832f3eb5e3d6ce0b
Paog04 <pablo20oligarcia@gmail.com>
2025-09-24 13:36:02 +0200

    Clase1

---
+++ app.java
@@ -0,0 +1,35 @@
+public class app {
+   /*  public static void hacernada() {
+        System.out.println("estoy en hacer nada");
+        int altura =2;
+        int diametro=5;
+        System.out.println("El area es (altura)*(diametro) ");
+    }
+    int fora=0;*/
+    /*public static void main(String[] args) throws Exception {
+        System.out.println("Hellow, word!");
+        //hacernada();
+    }*/
+    public static void main(String[] args) {
+        String cadena="me llamo Pablo";
+        int edad=25;
+        double salario=20.128234234;
+        /*int x=1;
+        int y=2;
+        int z=3;*/
+       /*  int x=1,y=2,z=3;
+
+        int variable=0;
+        System.out.println(variable);
+
+
+        variable=33;
+        System.out.println(variable);
+*/
+        int cuenta1= (1+2+3)/3;
+        double cuenta2= (1+2+3)/3.0;
+
+        System.out.println(cuenta1);
+        System.out.println(cuenta2);
+    }
+}
