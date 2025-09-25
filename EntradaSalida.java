d95b6a737e34cf316d571fe5197a042852d1edde
Paog04 <pablo20oligarcia@gmail.com>
2025-09-25 09:09:13 +0200

    clase2.1

--- EntradaSalida.java
+++ EntradaSalida.java
@@ -5,8 +5,30 @@ public class EntradaSalida {
         System.out.println(valor);
         //Usar printlf
         double precio=130.354789;
-        System.out.printf("El precio del producto %s tiene un valor de %5.2f y se han comprado %d", "Tablet", precio,valor);
+        System.out.printf("El precio del producto %s tiene un valor de %5.2f y se han comprado %d \n", "Tablet", precio,valor);
+        
         //Incluir icono
         System.out.println("Que ganas tengo de una vacaciones \u26F1");
+
+        //Formato reciente de System
+        System.out.println( "El precio del producto"+"tabloet"+"tiene un valor de "+precio+" y se han comprado "+valor);
+
+        //Formato salida texto
+        System.out.printf("%15s, %-15s","Pablo", "Informatico" );
+
+        System.out.println();
+        //Formato tabla
+        System.out.println("|----------|----------|------------|");
+        System.out.printf("|%-10s|%s  |%s       | \n","Producto", "Cantidad","Valor");
+        System.out.println("|----------|----------|------------|");
+        System.out.printf("|%-10s|%d         |%f  | \n","Producto",valor,precio);
+        System.out.println("|----------|----------|------------|");
+
+        System.out.println();
+        System.out.println("|----------|----------|----------|");
+        System.out.println("|"+"Producto"+"  |"+"Cantidad"+"  |    "+"Precio"+"|");
+        System.out.println("|----------|----------|----------|");
+        System.out.println("|"+"Producto  |       "+valor+"  |"+precio+"|");
+        System.out.println("|----------|----------|----------|");
     }
 }
--- app.java
+++ app.java
@@ -1,20 +1,16 @@
 public class app {
-    static final double PI=3.141592;//es una constante
- /*     public static void hacernada() {
+   /*  public static void hacernada() {
         System.out.println("estoy en hacer nada");
         int altura =2;
         int diametro=5;
         System.out.println("El area es (altura)*(diametro) ");
     }
-    int fora=0;
-    public static void main(String[] args) throws Exception {
+    int fora=0;*/
+    /*public static void main(String[] args) throws Exception {
         System.out.println("Hellow, word!");
         //hacernada();
-    }
- */
-    //public boolean static logico=true;
+    }*/
     public static void main(String[] args) {
-        //tipo de identificador = Valor
         String cadena="me llamo Pablo";
         int edad=25;
         double salario=20.128234234;
@@ -22,7 +18,6 @@ public class app {
         int y=2;
         int z=3;*/
        /*  int x=1,y=2,z=3;
-       Defino una variable y la imprimo
 
         int variable=0;
         System.out.println(variable);
@@ -30,40 +25,13 @@ public class app {
 
         variable=33;
         System.out.println(variable);
-        
+*/
         int cuenta1= (1+2+3)/3;
         double cuenta2= (1+2+3)/3.0;
-        int a=(int)2.6;
-        
-        System.out.println(a);
-    */
 
-        boolean logico=true; //false
-       // System.out.println(logico);
-
-        //int logico=1;//ya existe "logico" y no se puede utilizar 
-
-        
-       // PI=0.234555; //no se puede cambiar un aconstante
-        
-
-        int b='b';//un caracter se guarda en un enter segun codigo ASCII
-        char c=98;
-        //System.out.println(b+""+c);
-
-        System.out.println("un texto cualquiera");
-        System.err.println("un bonito error");
-
-        System.out.print("hola");
-        System.out.println("hola");
-    }
-
-    /**Progarama para imprimir texto
-     * @author Pablo Olivares García
-     * @param texto a imprimir
-     * @version 1.0
-     */
-    public static void imprimirtexto(String texto){
-        boolean logico=false;
+        System.out.println(cuenta1);
+        System.out.println(cuenta2);
     }
 }
+
+
