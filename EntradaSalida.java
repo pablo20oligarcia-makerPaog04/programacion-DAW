1960b49495ff64080bd79a50b4112c978db1ae52
Paog04 <pablo20oligarcia@gmail.com>
2025-09-25 08:30:29 +0200

    Clase2

---
+++ EntradaSalida.java
@@ -0,0 +1,12 @@
+public class EntradaSalida {
+    public static void main(String[] args){
+        //Instrucciones de salida por pantalla
+        int valor=7;
+        System.out.println(valor);
+        //Usar printlf
+        double precio=130.354789;
+        System.out.printf("El precio del producto %s tiene un valor de %5.2f y se han comprado %d", "Tablet", precio,valor);
+        //Incluir icono
+        System.out.println("Que ganas tengo de una vacaciones \u26F1");
+    }
+}
